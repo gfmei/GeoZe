6b017e388ec08e5809bbc28027f299ed9508e3ec
Guofeng Mei <meiguofeng@sina.cn>
2024-03-27 12:18:31 +0100

    first commit

---
+++ segmentator/csrc/build/CMakeFiles/libsegmentator.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libsegmentator.dir/segmentator.cpp.o"
+  "CMakeFiles/libsegmentator.dir/segmentator.cpp.o.d"
+  "libsegmentator.pdb"
+  "libsegmentator.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/libsegmentator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
